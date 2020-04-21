a0c171066fb980aa3f90a16d3d0bd0dc44a2a8aa
Allan Weber <a.cassianoweber@gmail.com>
2020-04-21 13:54:22 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'stack-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
